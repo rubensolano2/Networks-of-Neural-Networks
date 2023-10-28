30a7d9656a8a623a9b40fe53ca8d52fbc7120b82
Rubén Solano <105363958+rubensolano2@users.noreply.github.com>
2023-10-28 09:22:27 +0200

    Update Main-Code.py

--- Main-Code.py
+++ Main-Code.py
@@ -1,7 +1,14 @@
+# Import required libraries
+from sklearn.datasets import make_classification
+from torch.utils.data import DataLoader, TensorDataset
+from sklearn.model_selection import train_test_split
+import torch.optim as optim
+import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+# Define the NeuronNet and FinalInputFirstHiddenSkipConnectionNestedNeuralNetwork classes
 class NeuronNet(nn.Module):
     def __init__(self, input_size, hidden_size, output_size):
         super(NeuronNet, self).__init__()
@@ -13,38 +20,20 @@ class NeuronNet(nn.Module):
         x = self.fc2(x)
         return x
 
-class NestedNeuralNetwork(nn.Module):
-    def __init__(self, input_size, neuron_net_hidden_size, output_size, num_neurons):
-        super(NestedNeuralNetwork, self).__init__()
-        
-        # First hidden layer
-        self.fc1 = nn.Linear(input_size, num_neurons)
-        
-        # Second hidden layer with neuron-nets
-        self.neuron_nets = nn.ModuleList([NeuronNet(1, neuron_net_hidden_size, 1) for _ in range(num_neurons)])
-        
-        # Third hidden layer
-        self.fc3 = nn.Linear(num_neurons, output_size)
+class FinalInputFirstHiddenSkipConnectionNestedNeuralNetwork(nn.Module):
+    def __init__(self, input_size, hidden_size1, neuron_net_hidden_size, hidden_size3, output_size, num_neurons):
+        super(FinalInputFirstHiddenSkipConnectionNestedNeuralNetwork, self).__init__()
+        self.fc1 = nn.Linear(input_size, hidden_size1)
+        self.bn1 = nn.BatchNorm1d(hidden_size1)
+        self.neuron_nets = nn.ModuleList([NeuronNet(input_size + hidden_size1, neuron_net_hidden_size, 1) for _ in range(num_neurons)])
+        self.fc3 = nn.Linear(num_neurons, hidden_size3)
+        self.bn3 = nn.BatchNorm1d(hidden_size3)
+        self.fc4 = nn.Linear(hidden_size3, output_size)
 
     def forward(self, x):
-        # First layer
-        x = F.relu(self.fc1(x))
-        
-        # Second layer with neuron-nets
-        x = torch.cat([neuron_net(x[:, i].unsqueeze(1)) for i, neuron_net in enumerate(self.neuron_nets)], dim=1)
-        
-        # Third layer
-        x = self.fc3(x)
-        
-        return x
-
-# Initialize the model
-input_size = 10
-neuron_net_hidden_size = 5
-output_size = 1
-num_neurons = 3
-model = NestedNeuralNetwork(input_size, neuron_net_hidden_size, output_size, num_neurons)
-
-# Configure for CPU (can be easily moved to CUDA with .cuda())
-device = torch.device("cpu")
-model.to(device)
+        x1 = F.relu(self.bn1(self.fc1(x)))
+        skip_connection_input = torch.cat([x, x1], dim=1)
+        x2 = torch.cat([neuron_net(skip_connection_input) for neuron_net in self.neuron_nets], dim=1)
+        x3 = F.relu(self.bn3(self.fc3(x2)))
+        x4 = self.fc4(x3)
+        return x4
